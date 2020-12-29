5c27fc1a3c12a27e84cfe076e468b58509b9f282
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-12-29 22:39:03 +0530

    feat: remove the templates tab

--- packages/builder/components/pages/index/right-sidebar/tabs/index.tsx
+++ packages/builder/components/pages/index/right-sidebar/tabs/index.tsx
@@ -13,31 +13,12 @@ import {
 } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import React, { FC } from "react";
-import { MdChromeReaderMode, MdCollections, MdDns } from "react-icons/md";
+import { MdCollections, MdDns } from "react-icons/md";
 
 const LazyRightSidebarSectionsTab = dynamic(
   () =>
     import(
       /* webpackChunkName: 'lazyRightSidebarSectionsTab' */ "components/pages/index/right-sidebar/tabs/sections"
-    ),
-  {
-    ssr: false,
-    loading: () => {
-      return (
-        <Flex h={100} alignItems="center" justifyContent="center">
-          <Text fontSize="20" fontWeight="bold">
-            Loading...
-          </Text>
-        </Flex>
-      );
-    },
-  }
-);
-
-const LazyRightSidebarTemplatesTab = dynamic(
-  () =>
-    import(
-      /* webpackChunkName: 'lazyRightSidebarTemplatesTab' */ "components/pages/index/right-sidebar/tabs/templates"
     ),
   {
     ssr: false,
@@ -90,12 +71,6 @@ const RightSidebarTabs: FC = () => {
             <Text>Pages</Text>
           </HStack>
         </Tab>
-        <Tab>
-          <HStack spacing={1}>
-            <Icon as={MdChromeReaderMode} />
-            <Text>Templates</Text>
-          </HStack>
-        </Tab>
       </TabList>
       <TabPanels height="calc(100vh - 50px - 42px - 65px)" overflowY="scroll">
         <TabPanel>
@@ -106,11 +81,6 @@ const RightSidebarTabs: FC = () => {
         <TabPanel>
           <Box>
             <LazyRightSidebarPagesTab />
-          </Box>
-        </TabPanel>
-        <TabPanel>
-          <Box>
-            <LazyRightSidebarTemplatesTab />
           </Box>
         </TabPanel>
       </TabPanels>
